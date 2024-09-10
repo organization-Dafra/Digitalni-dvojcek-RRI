77a8de89c5fad891ed00b4a7398f98b2b4b75cab
Dejan Rožič <drozic1989@gmail.com>
2024-09-10 09:47:08 +0200

    Edit simple ads communicatgion

--- Python-Code/Python-Examples/SimpleAdsCommunication.py
+++ Python-Code/Python-Examples/SimpleAdsCommunication.py
@@ -1,25 +1,27 @@
 import pyads
 
 # Define connection details
-PLC_AMS_ID = '5.16.76.191.1.1'  # Replace with your PLC AMS Net ID
+PLC_AMS_ID = '5.87.67.53.1.1'  # Replace with your PLC AMS Net ID
 PLC_IP_ADDRESS = '192.168.0.100'  # Replace with your PLC IP address
 PLC_ADS_PORT = 851  # ADS port for PLC
 
-# Define variable names in the PLC
-angle_array_var = 'MAIN.angle'  # Replace with your array variable for angle
-voltage_array_var = 'MAIN.voltage'  # Replace with your array variable for voltage
 
-# Establish connection to the PLC
-plc = pyads.Connection(PLC_AMS_ID, PLC_ADS_PORT, PLC_IP_ADDRESS)
+# connect to plc and open connection
+# route is added automatically to client on Linux, on Windows use the TwinCAT router
+plc = pyads.Connection(PLC_AMS_ID, pyads.PORT_SPS1)
 plc.open()
+if plc.is_open:
+    print("plc is ok")
+else:
+    print("Comm. NOK")
 
-# Read array of angles
-angle_data = plc.read_by_name(angle_array_var, pyads.PLCTYPE_REAL * 360)  # Assuming 360 values in the array
-print("Angle Data:", angle_data)
 
-# Read array of voltages
-voltage_data = plc.read_by_name(voltage_array_var, pyads.PLCTYPE_REAL * 360)  # Assuming 360 values in the array
-print("Voltage Data:", voltage_data)
 
+i = plc.read_by_name("MAIN.testbool")
+print(i.tostring())
+# write int value by name
+plc.write_by_name("MAIN.test", 42.0)
 # Close the connection
 plc.close()
+
+
